4c1c7e1bec5b91d1adb8ed43121842fb69c0e802
Mahyar <mahyar.sh021@yahoo.com>
2022-11-09 16:49:07 +0330

    Project added.

--- ImageSet/ViewController.swift
+++ ImageSet/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imageView.backgroundColor = .secondarySystemBackground
     }
+    
+    @IBAction func selectImageButtonTapped(_ sender: Any) {
 
-
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary)
+        {
+            let picker = UIImagePickerController()
+            picker.sourceType = .photoLibrary
+            picker.delegate = self
+            present(picker, animated: true)
+        }
+    }
+    
+    @IBAction func captureButtonTapped() {
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.delegate = self
+        present(picker, animated: true)
+    }
 }
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as?
+                UIImage else {
+                    return
+                }
+        imageView.image = image
+    }
+}
