95895247a2cf6d17a32e5a27820bcc930c954bc5
Mahyar <mahyar.sh021@yahoo.com>
2022-11-09 16:13:13 +0330

    Initial Commit

---
+++ ImageSet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageSet
+//
+//  Created by MahyR Sh on 11/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
